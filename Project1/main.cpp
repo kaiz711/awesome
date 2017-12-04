e6fbc0b71216be31fc610b877a1d2ca312a872ff
DESKTOP-UN2K9O4\JohnKim <youngg662@naver.com>
2017-12-04 15:36:42 +0900

    test change

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,14 +2,19 @@
 
 SDL_Surface *background = NULL;
 SDL_Surface *screen = NULL;
+SDL_Surface* title_message = NULL;
 SDL_Surface* message = NULL;
 SDL_Surface* message2 = NULL;
 SDL_Surface* player = NULL;
 SDL_Surface* player2 = NULL;
 SDL_Surface* ball = NULL;
+SDL_Surface* heart = NULL;
+SDL_Surface* heart2 = NULL;
+SDL_Surface* heart3 = NULL;
 
 SDL_Event event;
 TTF_Font *font = NULL;
+TTF_Font *font2 = NULL;
 
 SDL_Rect balls[MAX_BALLS];
 
